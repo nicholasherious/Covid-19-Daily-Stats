6d8118ff680140a66c34769db54c823d4ee040d7
nicholasherious <nherious@gmail.com>
2020-04-17 00:03:07 -0700

    fix spelling error

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,14 @@ function Search() {
   return (
     <div className="container mt-4">
       <h1>
-        <p class="text-center">Gloval Covid-19 Stats</p>
+        <p class="text-center">Global Covid-19 Stats</p>
       </h1>
       <p class="lead text-center">
-        Data is sourced from <a href="https://github.com/CSSEGISandData/COVID-19">Johns Hopkins CSSE</a> and updated every 10 minutes.
+        Data is sourced from{' '}
+        <a href="https://github.com/CSSEGISandData/COVID-19">
+          Johns Hopkins CSSE
+        </a>{' '}
+        and updated every 10 minutes.
       </p>
     </div>
   );
