44de6fbe3f4daa3431f99682b3069e5ce78a5629
nicholasherious <nherious@gmail.com>
2020-04-16 21:46:59 -0700

    removed navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Main from './components/Main';
-import Navbar from './components/Navbar';
 import CountryList from './components/CountryList';
 import Search from './components/Search';
 
